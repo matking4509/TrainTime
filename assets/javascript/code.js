f70e92a8ae7685e40c65b8dbb1e116194cf97f44
Mat <mat@zoidberg.localdomain>
2019-01-07 08:11:33 -0500

    stuff

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -9,41 +9,79 @@
   };
   firebase.initializeApp(config);
   var database = firebase.database();
-
-
-
-// Watch for trains being added and paint the list
-// Link to data on pulling from unknown parent ref.
-// https://stackoverflow.com/questions/38541098/how-to-retrieve-data-from-firebase-database
-
-var leadsRef = database.ref('trains');
-leadsRef.on('child_added', function(snapshot) {
-    // for (var i=0; i < snapshot.length; i++) {
-    //   var childData = snapshot[i].val();
-    //   console.log("TrainID-" + i, childData);
-    // }
-    snapshot.forEach(function(childSnapshot) {
-      var childData = childSnapshot.val();
-      console.log("TrainID", childData);
-    });
-});
-
-
+  
+  displayTrains();
+  intervalId = setInterval(displayTrains, 60000);
+// Refresh Trains List
+  $("#trainReload").on("click", displayTrains);
 
   // Watch for Train Entry Form.
   $("#add-train").on("click", function(event) {
     // Don't refresh the page!
+    var submitFlag = 0;
     event.preventDefault();
-    trainID = Math.floor((new Date).getTime() / 1000);
     trainName = $("#trainName-input").val().trim();
     trainDest = $("#trainDest-input").val().trim();
     trainFat = $("#trainFat-input").val().trim();
+        var reFat = new RegExp("[0-9][0-9]:[0-9][0-9]");
+        if (reFat.test(trainFat)) {
+            console.log("Valid FAT");
+        } else {
+            alert("Please enter valid First Arriaval time. 24-Hour format ex: 14:00");
+            submitFlag = 1;
+        }   
+    //validate Input
     trainFreq = $("#trainFreq-input").val().trim();
-
-    database.ref("/trains/" + trainID).update({
-      train_name: trainName,
-      train_destination: trainDest,
-      train_firstarrivaltime: trainFat,
-      train_frequency: trainFreq
-    });
+        var reFreq = new RegExp('\d+');
+        if (reFreq.test(trainFat)) {
+            console.log("Valid Freq");
+        } else {
+            alert("Please enter valid Frequency. Numbers Only");
+            submitFlag = 1;
+        }   
+    //validate Input
+    if (submitFlag = 0 ) {
+        database.ref("/trains").push({
+            train_name: trainName,
+            train_destination: trainDest,
+            train_firstarrivaltime: trainFat,
+            train_frequency: trainFreq
+        });
+    }
+    displayTrains();
   });
+
+  function displayTrains() {
+    $("#trainData").replaceWith("<table id=\"trainData\" style=\"width:100%\"><tr><th>Train Name</th><th>Destination</th><th>Frequency</th><th>Next Arrival</th><th>Minutes Away</th></tr></table>");
+    var leadsRef = database.ref('trains');
+        leadsRef.on('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+    var childData = childSnapshot.val();
+        
+    var firstTimeConverted = moment(childData.train_firstarrivaltime, "HH:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % childData.train_frequency;
+    var tMinutesTillTrain = childData.train_frequency - tRemainder;
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    
+    var newRow = $("<tr>");
+    var tableName = $("<td>");
+        tableName.text(childData.train_name);
+        tableName.appendTo(newRow);
+    var tableDest = $("<td>");
+        tableDest.text(childData.train_destination);
+        tableDest.appendTo(newRow);
+    var tableFreq = $("<td>");
+        tableFreq.text(childData.train_frequency);
+        tableFreq.appendTo(newRow);
+    var tableNat = $("<td>");
+        tableNat.text(moment(nextTrain).format("hh:mm"))
+        tableNat.appendTo(newRow);
+    var tableMinAway = $("<td>");
+        tableMinAway.text(tMinutesTillTrain);
+        tableMinAway.appendTo(newRow);
+        newRow.appendTo("#trainData");
+        // console.log("childData", childData);
+      });
+  });
+  }
