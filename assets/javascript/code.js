a51f4348d2fce25e45a26c8126b86f783f47bd22
Mat King <mat@matking.info>
2019-01-08 14:11:35 -0500

    stuff

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -18,29 +18,37 @@
   // Watch for Train Entry Form.
   $("#add-train").on("click", function(event) {
     // Don't refresh the page!
-    var submitFlag = 0;
     event.preventDefault();
+    var submitFlag = 0;
     trainName = $("#trainName-input").val().trim();
+        if (!trainName) {
+            alert("Please enter a Train Name:");
+            submitFlag = 1;
+        }
     trainDest = $("#trainDest-input").val().trim();
+        if (!trainDest) {
+            alert("Please enter a Destination:");
+            submitFlag = 1;
+        }
     trainFat = $("#trainFat-input").val().trim();
         var reFat = new RegExp("[0-9][0-9]:[0-9][0-9]");
-        if (reFat.test(trainFat)) {
-            console.log("Valid FAT");
-        } else {
+        if (!reFat.test(trainFat) || !trainFat) {
             alert("Please enter valid First Arriaval time. 24-Hour format ex: 14:00");
             submitFlag = 1;
-        }   
-    //validate Input
-    trainFreq = $("#trainFreq-input").val().trim();
-        var reFreq = new RegExp('\d+');
-        if (reFreq.test(trainFat)) {
-            console.log("Valid Freq");
         } else {
+           // console.log("Valid FAT");
+        }   
+    // //validate Input
+    trainFreq = $("#trainFreq-input").val().trim();
+        if (trainFreq.match(/[a-z]/i) || !trainFreq) {
             alert("Please enter valid Frequency. Numbers Only");
             submitFlag = 1;
+        } else {
+            // console.log("Valid Freq");
         }   
-    //validate Input
-    if (submitFlag = 0 ) {
+    // //validate Input
+
+    if (submitFlag === 0 ) {
         database.ref("/trains").push({
             train_name: trainName,
             train_destination: trainDest,
@@ -51,37 +59,37 @@
     displayTrains();
   });
 
-  function displayTrains() {
+function displayTrains() {
     $("#trainData").replaceWith("<table id=\"trainData\" style=\"width:100%\"><tr><th>Train Name</th><th>Destination</th><th>Frequency</th><th>Next Arrival</th><th>Minutes Away</th></tr></table>");
     var leadsRef = database.ref('trains');
         leadsRef.on('value', function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-    var childData = childSnapshot.val();
-        
-    var firstTimeConverted = moment(childData.train_firstarrivaltime, "HH:mm").subtract(1, "years");
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    var tRemainder = diffTime % childData.train_frequency;
-    var tMinutesTillTrain = childData.train_frequency - tRemainder;
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    
-    var newRow = $("<tr>");
-    var tableName = $("<td>");
-        tableName.text(childData.train_name);
-        tableName.appendTo(newRow);
-    var tableDest = $("<td>");
-        tableDest.text(childData.train_destination);
-        tableDest.appendTo(newRow);
-    var tableFreq = $("<td>");
-        tableFreq.text(childData.train_frequency);
-        tableFreq.appendTo(newRow);
-    var tableNat = $("<td>");
-        tableNat.text(moment(nextTrain).format("hh:mm"))
-        tableNat.appendTo(newRow);
-    var tableMinAway = $("<td>");
-        tableMinAway.text(tMinutesTillTrain);
-        tableMinAway.appendTo(newRow);
-        newRow.appendTo("#trainData");
-        // console.log("childData", childData);
-      });
-  });
-  }
+            snapshot.forEach(function(childSnapshot) {
+                var childData = childSnapshot.val();
+
+                var firstTimeConverted = moment(childData.train_firstarrivaltime, "HH:mm").subtract(1, "years");
+                var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+                var tRemainder = diffTime % childData.train_frequency;
+                var tMinutesTillTrain = childData.train_frequency - tRemainder;
+                var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+                
+                var newRow = $("<tr>");
+                var tableName = $("<td>");
+                    tableName.text(childData.train_name);
+                    tableName.appendTo(newRow);
+                var tableDest = $("<td>");
+                    tableDest.text(childData.train_destination);
+                    tableDest.appendTo(newRow);
+                var tableFreq = $("<td>");
+                    tableFreq.text(childData.train_frequency);
+                    tableFreq.appendTo(newRow);
+                var tableNat = $("<td>");
+                    tableNat.text(moment(nextTrain).format("hh:mm"))
+                    tableNat.appendTo(newRow);
+                var tableMinAway = $("<td>");
+                    tableMinAway.text(tMinutesTillTrain);
+                    tableMinAway.appendTo(newRow);
+                    newRow.appendTo("#trainData");
+                    // console.log("childData", childData);
+                });
+        });
+}
